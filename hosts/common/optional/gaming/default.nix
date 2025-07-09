9855c4c1ad04660981efacbb0ba4604a1ce6d884
Asaduzzaman pavel <contact@iampavel.dev>
2025-07-09 14:27:07 +0600

    Feat: Add more game launcher

--- hosts/common/optional/gaming/default.nix
+++ hosts/common/optional/gaming/default.nix
@@ -1,6 +1,54 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  imports = [
-    ./steam.nix
+  programs = {
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      gamescopeSession.enable = true;
+      extraCompatPackages = [ pkgs.proton-ge-bin ];
+    };
+
+    gamescope = {
+      enable = true;
+      capSysNice = true;
+      args = [
+        "--rt"
+        "--expose-wayland"
+      ];
+    };
+
+    # To run steam games in game mode, add the following to the game's properties from within steam
+    # gamemoderun %command%
+    gamemode = {
+      enable = true;
+      settings = {
+        #see gamemode man page for settings info
+        general = {
+          softrealtime = "on";
+          inhibit_screensaver = 1;
+        };
+        gpu = {
+          apply_gpu_optimisations = "accept-responsibility";
+          gpu_device = 1; # The DRM device number on the system (usually 0), ie. the number in /sys/class/drm/card0/
+        };
+        custom = {
+          start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+          end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+        };
+      };
+    };
+  };
+
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
+  };
+
+  # environment.systemPackages = with pkgs; [mangohud protonup-qt lutris bottles heroic];
+  environment.systemPackages = with pkgs; [
+    mangohud
+    protonup-qt
+    lutris
+    bottles
   ];
 }
--- rsync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-HOST="192.168.1.145"
-
-rsync -davz . "nixos@${HOST}:/home/nixos/nix-config/"
-while inotifywait -r -e modify,create,delete .; do
-  rsync -davz . "nixos@${HOST}:/home/nixos/nix-config/"
-done
